60e91ffb77f4ea735f14e71a7297502188f29bcd
iRewiewer <irewiewer@gmail.com>
2020-11-14 23:34:06 +0200

    Updated cfg

--- helpcfg.bat
+++ helpcfg.bat
@@ -8,17 +8,11 @@ echo :: [Aliases]                                   ::
 echo ::                                             ::
 echo :: ls = displays current directory             ::
 echo :: clear = clears screen                       ::
-echo :: green/red/blue/purple_text = colors screen  ::
 echo ::                                             ::
 echo :: [Scripts]                                   ::
 echo ::                                             ::
 echo :: helpcfg = displays this message             ::
 echo :: edithelpcfg = edits this messsage           ::
 echo :: aliascfg = starts the alias loader          ::
-echo :: wincfg = starts window tweaker app          ::
-echo :: scw = starts cmd in pyenv cw1 folder        ::
-echo :: roti = prints true whatever comes after it  ::
 echo ::                                             ::
 echo :::::::::::::::::::::::::::::::::::::::::::::::::
-
-"G:\Dropbox Stuff\Code\DOSKEY Manager\text.bat"
