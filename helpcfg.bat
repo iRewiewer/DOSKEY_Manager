c5aadd2d902d9a1256cf12a7e33030c10866cb4c
iRewiewer <irewiewer@gmail.com>
2021-11-23 17:04:24 +0200

    Update helpcfg.bat

--- helpcfg.bat
+++ helpcfg.bat
@@ -2,17 +2,21 @@
 
 cls
 echo.
-echo :::::::::::::::::::: HelpCFG ::::::::::::::::::::
-echo ::                                             ::
-echo :: [Aliases]                                   ::
-echo ::                                             ::
-echo :: ls = displays current directory             ::
-echo :: clear = clears screen                       ::
-echo ::                                             ::
-echo :: [Scripts]                                   ::
-echo ::                                             ::
-echo :: helpcfg = displays this message             ::
-echo :: edithelpcfg = edits this messsage           ::
-echo :: aliascfg = starts the alias loader          ::
-echo ::                                             ::
-echo :::::::::::::::::::::::::::::::::::::::::::::::::
+echo [31m:::::::::::::::::::: [33mHelpCFG [31m::::::::::::::::::::[0m
+echo [31m::                                             ::[0m
+echo [31m:: [33m[CMD Settings]                              [31m::[0m
+echo [31m::                                             ::[0m
+echo [31m:: [91mhelpcfg [35m= [32mdisplays this message             [31m::[0m
+echo [31m:: [91medithelpcfg [35m= [32medits this messsage           [31m::[0m
+echo [31m:: [91maliascfg [35m= [32mstarts the alias loader          [31m::[0m
+echo [31m:: [91mwelcomecfg [35m= [32medits the welcome message      [31m::[0m
+echo [31m::                                             ::[0m
+echo [31m::                                             ::[0m
+echo [31m:: [33m[Scripts]                                   [31m::[0m
+echo [31m::                                             ::[0m
+echo [31m:: [91mls [35m= [32mdisplays current directory             [31m::[0m
+echo [31m:: [91mclear [35m= [32mclears screen                       [31m::[0m
+echo [31m:: [91mpwd [35m= [32mcopies the current path to clipboard  [31m::[0m
+echo [31m:: [91mcat [file] [35m= [32mprints file contents           [31m::[0m
+echo [31m::                                             ::[0m
+echo [31m:::::::::::::::::::::::::::::::::::::::::::::::::[0m
