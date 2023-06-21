b0fa0f13ddfe0618fe3365140fb260097db3f54f
iRewiewer <irewiewer@gmail.com>
2023-06-22 00:16:52 +0300

    removed space typo

--- helpcfg.bat
+++ helpcfg.bat
@@ -11,8 +11,8 @@ echo [31m::                                                             ::[0m
 echo [31m:: [91mhcfg              [35m= [32mdisplays this message                   [31m::[0m
 echo [31m:: [91medit_hcfg         [35m= [32mopens helpcfg.bat                       [31m::[0m
 echo [31m:: [91mcfg               [35m= [32mopens the DOSKEY_Loader.bat             [31m::[0m
-echo [31m:: [91mwcfg               [35m= [32mopens the welcome.txt                  [31m::[0m
-echo [31m:: [91mversion            [35m= [32mprints out the DOSKEY Manager version  [31m::[0m
+echo [31m:: [91mwcfg              [35m= [32mopens the welcome.txt                   [31m::[0m
+echo [31m:: [91mversion           [35m= [32mprints out the DOSKEY Manager version   [31m::[0m
 ::-------------------------------------------------------------------------------------------------------------::
 echo [31m::                                                             ::[0m
 echo [31m:: [33m[Aliases]                                                   [31m::[0m
@@ -35,7 +35,10 @@ echo [31m::                                                             ::[0m
 echo [31m:: [33m[App Shortcuts]                                             [31m::[0m
 echo [31m::                                                             ::[0m
 ::-------------------------------------------------------------------------------------------------------------::
+echo [31m:: [91mwincfg            [35m= [32mstarts window tweaker app               [31m::[0m
+echo [31m:: [91mlolskins          [35m= [32mopens the txt with all lol skins        [31m::[0m
 echo [31m:: [91mviewColors        [35m= [32mview all cmd colors file                [31m::[0m
+echo [31m:: [91mgrades            [35m= [32mview uni grades from all years          [31m::[0m
 ::-------------------------------------------------------------------------------------------------------------::
 echo [31m::                                                             ::[0m
 echo [31m:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::[0m
